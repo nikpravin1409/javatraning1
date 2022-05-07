010c3ce04b972e1bbb648f36ed6999661359468e
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-07 13:18:49 +0530

    Initial comment

---
+++ day1/demo1.java
@@ -0,0 +1,59 @@
+// // class demo1 {
+// // public static void main(String[] args){
+// // System.out.println("Hello world");
+// // System.out.println("pravin nikam");
+// // System.out.print("narayan");
+// // }
+// // }
+// // 
+// // 
+// // 
+// // 
+// // 
+// // class pravin{
+// // public static void main(String[] args){
+// // System.out.println("p b y k g y ");
+// // }
+// // }
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// class pravi{
+// public static void main(String[] args){
+// System.out.println("hello india");
+// }
+// }
+
+
+
+// class pravi{
+// public static void main(String[] args){
+// 
+// 
+// float x;
+// double y;
+// x=2f;
+// y=1.3;
+// System.out.println(x);
+// System.out.println(y);
+// }
+// }
+
+
+
+
+class pr{
+public static void main(String[] args){
+String name;
+name="abc";
+System.out.println("HELLO"+" "+name);
+}
+}
