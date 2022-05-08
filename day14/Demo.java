4b25e7b939b7a01454611bdcb7c937292c9d5d18
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-08 14:32:25 +0530

    Initial comment

---
+++ day14/Demo.java
@@ -0,0 +1,18 @@
+//ByteArrayInputStream
+import java.io.*;
+class Demo{
+    public static void main(String[] args) {
+    
+        String msg="देवगिरी";
+        byte[] arr=msg.getBytes();
+        ByteArrayInputStream ob;
+        ob=new ByteArrayInputStream(arr);
+        int x;
+        do{ 
+            x=ob.read();
+            System.out.print((char)x);
+
+        }while(x!=-1);
+        System.out.println();
+    }
+}
