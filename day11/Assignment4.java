0d3722fa13988806edb02c1aa6828da630cc665c
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-08 14:28:25 +0530

    Initial comment

---
+++ day11/Assignment4.java
@@ -0,0 +1,20 @@
+// Given a string of even length, return the first half. So the string "CatDog" yields "Cat". If the
+// string length is odd number then return null.
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		String str = "CatDogs";
+		
+		String output;
+		
+		if (str.length() % 2 == 0) {
+			output = str.substring(0, str.length() / 2);
+		} else {
+			output = null;
+		}
+		
+		System.out.println(output);
+	}
+
+}
---
+++ day11/Assingment6.java
@@ -0,0 +1,4 @@
+// Return a version of the given string, where for every star () in the string the star and the chars
+// immediately to its left and right are gone. So "abcd" yields "ad" and "ab**cd" also yields "ad".
+
+
