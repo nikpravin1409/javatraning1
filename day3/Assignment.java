a62b148ba749a11879acc1027c90998b73240d89
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-08 14:11:23 +0530

    Initial comment

---
+++ day3/Assignment.java
@@ -0,0 +1,7 @@
+public class Demo7{
+public statix void main(String[] args){
+Scanner sc;
+sc=new Scanner(System.in);
+int x;
+x=sc,nextInt();
+k=3:
---
+++ day3/Shortfall.java
@@ -0,0 +1,8 @@
+public class Shortfall
+{
+public static void main (String agrs[]){
+
+short value = 32;
+System.out.println("A short: " + value);
+}
+}
