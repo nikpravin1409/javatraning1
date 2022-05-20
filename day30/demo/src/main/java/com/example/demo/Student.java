cb9c8a97675c0096a08ea001cfc9a6aa421aa36c
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-20 12:59:31 +0530

    Initial commit

--- day30/demo/src/main/java/com/example/demo/Student.java
+++ day30/demo/src/main/java/com/example/demo/Student.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+// import java.util.*
 class Scanner{
 public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
+// Scanner sc = new Scanner(System.in);
+// 
+// 
+// System.out.println("Enter first no.");
+// int a =sc.nextInt();
+// System.out.println("Enter first second no.");
+// int b=sc.nextInt();
+// int sum=a+b;
+// System.out.println("sum is");
+// System.out.println(sum);
+// 1)Qustion
+
+String name="PRAVIN NIKAM";
+name = name.toLowerCase();
+System.out.println(name);
 
 
-System.out.println("Enter first no.");
-int a =sc.nextInt();
-System.out.println("Enter first second no.");
-int b=sc.nextInt();
-int sum=a+b;
-System.out.println("sum is");
-System.out.println(sum);
 
 
 }
