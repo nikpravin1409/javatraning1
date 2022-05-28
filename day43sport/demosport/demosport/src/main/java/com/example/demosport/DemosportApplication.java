9097902472680077638df9e6b5ddb41b8c29df00
nikpravin1409 <96310351+nikpravin1409@users.noreply.github.com>
2022-05-28 12:20:46 +0530

    Initial commit

---
+++ day43sport/demosport/demosport/src/main/java/com/example/demosport/DemosportApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosportApplication.class, args);
+	}
+
+}
---
+++ day43sport/demosport/demosport/src/test/java/com/example/demosport/DemosportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
